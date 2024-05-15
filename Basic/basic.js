cf125c876b25afdaf6dc61c89f9b5ede6aaf486a
Sanjana <sanjana6122002@gmail.com>
2024-05-15 13:04:49 +0530

    basic

--- Basic/basic.js
+++ Basic/basic.js
@@ -1,4 +1,8 @@
 console.log('Hello World!'); 
+// Output: Hello world!
+
+
+// *******Declaring Variables********* 
 
 const accountName = 'Sanju'
 const accountId = 12352;
@@ -10,13 +14,28 @@ var accountCity ='gorakhpur'
 // --- if we want to change the value of any variable ---
 accountCity = 'lucknow'      
 console.log(accountCity); 
+// Output: lucknow
 
 // --- for printing values ---
 console.log(accountName);
 console.log(accountId);     
 console.log(accountEmail);
 console.log(accountPass);
+// Output:Sanju
+// Output:12352
+// Output:sanju@gmail.com 
+// Output:2421
 
 // --- for printing the values in table form ---
-console.table([accountName,accountId,accountPass,accountCity,accountEmail]);  
+console.table([accountName,accountId,accountPass,accountCity,accountEmail]); 
 
+// Output:
+// ┌─────────┬───────────────────┐
+// │ (index) │      Values       │
+// ├─────────┼───────────────────┤
+// │    0    │      'Sanju'      │
+// │    1    │       12352       │
+// │    2    │      '2421'       │
+// │    3    │     'lucknow'     │
+// │    4    │ 'sanju@gmail.com' │
+// └─────────┴───────────────────┘
--- Basic/datatype.js
+++ Basic/datatype.js
@@ -1,4 +1,4 @@
-//  Datatype Conversion Confusion
+//  ******************Datatype Conversion Confusion**********************
 
 // --- for checking the type of values ---
 
@@ -10,19 +10,37 @@ console.log(typeof 20);
 console.log(typeof state);
 console.log(typeof null);
 console.log(undefined);
+// Output:
+// string
+// number
+// undefined
+// object
+// undefined
+
 
 // --- for changing the type of variable ---
 
 let score = '33'
 console.log(score);
+// Output:33
 console.log(typeof score);
+// Output:string
+
 
 let newScore = Number(score);
 console.log(newScore);
+// Output:33
 console.log(typeof newScore);
+// Output:number
+
 
 let isLoggedIn = 1
 console.log(typeof isLoggedIn);
+// Output:number
+
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
+// Output:true
+console.log(typeof booleanIsLoggedIn);
+// Output: boolean 
