5c7e553d627e04ce212112f59013056ca6f463fc
Sanjana <sanjana6122002@gmail.com>
2024-05-17 14:53:09 +0530

    basic

---
+++ Basic/04_increment.js
@@ -0,0 +1,23 @@
+// *****************Postfix Increment ***********************
+
+let x = 3;
+const y = x++;
+console.log(x,y);
+// Outpu: 4 3
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+// Outpu: 101
+
+// ****************Prefix Increment ************************
+
+let X = 3;
+const Y = ++X;
+console.log(X,Y);
+// Outpu: 4 3
+
+let gameCounter2 = 100;
+++gameCounter2;
+console.log(gameCounter2);
+// Outpu: 101
---
+++ Basic/05_comparision.js
@@ -0,0 +1,50 @@
+// ********************Double Equal ( == )************************
+
+var a = 12;
+var b = '12';
+console.log( a == b);
+// Output: true 
+
+
+// *********************Triple Equal ( === )************************
+
+var a = 121;
+var b = '121';
+console.log( a === b);
+// Output: false
+
+
+// ***********************Comparision Operator**********************
+
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 1);
+console.log("2" === 2);
+console.log(2 != 1);
+console.log("2" > 1);
+console.log("02" > 1);
+// Output: 
+// true
+// true
+// false
+// false
+// false
+// true
+// true
+// true
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// Output: 
+// false
+// false
+// true
+// false
+// false
+// false
