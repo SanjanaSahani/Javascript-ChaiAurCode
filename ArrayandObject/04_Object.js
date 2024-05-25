fdf1905df231e4f44225852ceec2d931fc0c244a
Sanjana <sanjana6122002@gmail.com>
2024-05-25 23:24:35 +0530

    ArrayandObject

---
+++ ArrayandObject/04_Object.js
@@ -0,0 +1,14 @@
+const tinderUser = new Object()
+tinderUser.id = "123abc"
+tinderUser.name = "Sammy"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+//Output: { id: '123abc', name: 'Sammy', isLoggedIn: false }
+
+const tinderUser2 = {}
+tinderUser2.id = "123abcd"
+tinderUser2.name = "Sam"
+tinderUser2.isLoggedIn = true
+console.log(tinderUser2);
+//Output: { id: '123abcd', name: 'Sam', isLoggedIn: true }
