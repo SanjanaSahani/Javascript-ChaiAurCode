7aa33da58a063a88ee83769660706ca1c6af4493
Sanjana Sahani <137151823+SanjanaSahani@users.noreply.github.com>
2025-02-20 14:38:36 +0530

    random

--- InterviewCodingQuestions/Random.js
+++ InterviewCodingQuestions/Random.js
@@ -8,3 +8,60 @@ console.log(f(5)(10));
 //Output - 15
 
 
+// 2. How would you use a closure to create a private counter
+function counter(){
+    var _counter = 0
+    function add(increment){
+        _counter += increment
+    }
+    function retrive(){
+       return _counter;
+    }
+    return { add, retrive}
+}
+const c = counter();
+c.add(5);
+c.add(10);
+console.log(c.retrive());
+//Output - 15
+
+
+// 3. Make this function run only once
+let views;
+function likeMe(){
+    let called = 0;
+    return function(){
+        if(called > 0){
+            console.log("Already Subscribed to ", views);  
+        } else{
+            views = "Sanjana";
+            console.log(" Subscribed to ", views);  
+            called++;
+        }
+    }
+}
+let like = likeMe()
+like();
+like();
+like();
+like();
+//Output-  
+// Subscribed to  Sanjana
+// Already Subscribed to  Sanjana
+// Already Subscribed to  Sanjana
+// Already Subscribed to  Sanjana
+
+
+// 4. write a code for the perfect number
+function perfectNumber(number){
+let sum = 0;
+for(let i = 0; i<=number/2; i++){
+    if(number%i === 0){
+        sum += i
+    }
+}
+return sum === number
+}
+console.log(perfectNumber(28));
+//Output- true
+
