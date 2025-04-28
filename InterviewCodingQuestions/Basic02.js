fb876babcf1fdd11aa680960e0c21b9b7ad4d9d0
Sanjana Sahani <137151823+SanjanaSahani@users.noreply.github.com>
2025-04-28 17:40:52 +0530

    Basic02

---
+++ InterviewCodingQuestions/Basic02.js
@@ -0,0 +1,20 @@
+// 1. How do you reverse a string?
+function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+  // Example
+  console.log(reverseString("hello")); 
+
+// Output : "olleh"
+
+
+// How do you determine if a string is a palindrome?
+function isPalindrome(str) {
+    const reversed = str.split('').reverse().join('');
+    return str === reversed;
+  }
+  
+  // Example
+  console.log(isPalindrome("racecar")); 
+
+// Output : true
