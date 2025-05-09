f82835536dfc114e7468ee9c637e8d4a8b2ee410
Sanjana Sahani <137151823+SanjanaSahani@users.noreply.github.com>
2025-05-09 14:00:03 +0530

    Basic02.js

--- InterviewCodingQuestions/Basic02.js
+++ InterviewCodingQuestions/Basic02.js
@@ -18,3 +18,46 @@ function isPalindrome(str) {
   console.log(isPalindrome("racecar")); 
 
 // Output : true
+
+// 3. How do you calculate the number of numerical digits in a string?
+
+function countDigits(str) {
+  return (str.match(/\d/g) || []).length;
+}
+
+// Example
+console.log(countDigits("abc123def45")); 
+// Output:5
+
+
+// 4. How do you find the count for the occurrence of a particular character in a string?
+
+function countCharacter(str, char) {
+  return str.split('').filter(c => c === char).length;
+}
+
+// Example
+console.log(countCharacter("hello world", "l"));
+// Output:3
+
+// 5. How do you find the non-matching characters in a string?
+
+function nonMatchingChars(str1, str2) {
+  const set1 = new Set(str1);
+  const set2 = new Set(str2);
+  const result = [];
+
+  set1.forEach(char => {
+    if (!set2.has(char)) result.push(char);
+  });
+
+  set2.forEach(char => {
+    if (!set1.has(char)) result.push(char);
+  });
+
+  return result;
+}
+
+// Example
+console.log(nonMatchingChars("abc", "bcd"));
+// Output:[ 'a', 'd' ]
