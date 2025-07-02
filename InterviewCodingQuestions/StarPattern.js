6df42355699b5083caf989488efee06752da3d24
Sanjana Sahani <137151823+SanjanaSahani@users.noreply.github.com>
2025-07-02 13:47:47 +0530

    StarPattern.js

--- InterviewCodingQuestions/StarPattern.js
+++ InterviewCodingQuestions/StarPattern.js
@@ -117,3 +117,28 @@ InvertedLeftAngle(5);
 //   ***
 //    **
 //     * 
+
+// 8. Print a hollow square of stars with n rows and columns
+
+function hollowSquare(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = '';
+    for (let j = 1; j <= n; j++) {
+      if (i === 1 || i === n || j === 1 || j === n) {
+        row += '*';
+      } else {
+        row += ' ';
+      }
+    }
+    console.log(row);
+  }
+}
+
+hollowSquare(5);
+
+// Output:
+// *****
+// *   *
+// *   *
+// *   *
+// *****
